d9a07121550ade93a4ff78ef628dbe3ac3ae48d1
Neasu <housefan97@yahoo.de>
2013-12-08 18:04:53 +0100

    Jetzt ist Core wieder da

---
+++ GOI31 Desktop/src/core/Core.java
@@ -0,0 +1,32 @@
+package core;
+
+import javax.swing.JOptionPane;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
+
+import org.json.JSONObject;
+
+import server.Request;
+import server.Response;
+import server.ServerExecutor;
+
+import gui.GUIManager;
+
+public class Core {
+
+        public static void main(String[] args) throws ClassNotFoundException, InstantiationException, IllegalAccessException, UnsupportedLookAndFeelException {
+                UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+                
+                // TODO Auto-generated method stub
+                Request req = new Request("login", "login", new JSONObject());
+                
+                Response resp = ServerExecutor.ExecuteRequest(req);
+                
+                JOptionPane.showMessageDialog(null, resp.getPlainResponse(), "Information", JOptionPane.PLAIN_MESSAGE);
+        
+				// N3asu
+				
+				new GUIManager ().Init ();
+		}
+
+}
